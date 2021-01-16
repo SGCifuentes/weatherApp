fa668ed584eb49ea707ff7e9601b052939695c18
Sebastian Gonzalez <sebastiangc19.01@gmail.com>
2021-01-15 21:00:44 -0500

    add cards with js

--- js/index.js
+++ js/index.js
@@ -2,7 +2,8 @@ const inputText = document.getElementById("city-name"),
   btn = document.getElementById("btn-add-city"),
   form = document.querySelector(".add-city--form"),
   siteCards = document.querySelector(".site__cards"),
-  apiKey = "6e27421993a4ce79495c7afe6cdbec86";
+  apiKey = "6e27421993a4ce79495c7afe6cdbec86",
+  msg = document.createTextNode("Please write a valid city 😩");
 
 const addCity = function () {
   const city = this.previousElementSibling.value;
@@ -18,7 +19,6 @@ const addCity = function () {
       if (form.lastElementChild.tagName !== "P") {
         const p = document.createElement("p");
         p.classList.add("error");
-        const msg = document.createTextNode("Please write a valid city 😩");
         p.appendChild(msg);
         form.append(p);
       }
@@ -27,16 +27,60 @@ const addCity = function () {
   fetchData();
 };
 
+function newCard(weather, main, sys, name) {
+  const card = document.createElement("div"),
+    city = document.createElement("p"),
+    cityText = document.createTextNode(`${name} `),
+    country = document.createElement("span"),
+    countryText = document.createTextNode(sys["country"]),
+    tempContent = document.createElement("div"),
+    temp = document.createElement("p"),
+    tempValue = Math.round(main["temp"]),
+    tempText = document.createTextNode(tempValue),
+    celsius = document.createElement("sup"),
+    celsiusText = document.createTextNode("°C"),
+    img = document.createElement("img"),
+    description = document.createElement("p"),
+    descriptionText = document.createTextNode(weather[0]["description"]);
+
+  card.classList.add("card");
+  city.classList.add("city");
+  country.classList.add("country");
+  tempContent.classList.add("temp");
+  img.setAttribute(
+    "src",
+    `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${weather[0]["icon"]}.svg`
+  );
+  img.setAttribute("alt", "icon");
+
+  temp.appendChild(tempText);
+  celsius.appendChild(celsiusText);
+  temp.append(celsius);
+  description.appendChild(descriptionText);
+  tempContent.append(temp);
+  tempContent.append(img);
+  tempContent.append(description);
+  city.appendChild(cityText);
+  country.appendChild(countryText);
+  city.append(country);
+  card.append(city);
+  card.append(tempContent);
+
+  siteCards.append(card);
+}
+
 const createCard = function (data) {
-  console.log(`data => ${data.name}`);
-  console.log(data);
-  const msg = document.querySelector('.error');
+  const { main, name, sys, weather } = data;
+
+  newCard(weather, main, sys, name);
 
   msg.textContent = "";
+  if (form.length > 2) {
   form.removeChild(msg);
+  }
   form.reset();
   inputText.focus();
-}
+};
 
 //Event
 btn.addEventListener("click", addCity);
